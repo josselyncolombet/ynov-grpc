016acc15bd09cab30a56bdffbcbdd88ed87bdca2
Joss <joss@MacBook-Pro-de-Joss.local>
2023-11-21 10:03:25 +0100

    add clients

--- client.js
+++ client.js
@@ -1,17 +1,30 @@
-const grpc = require('grpc');
-const protoLoader = require('@grpc/proto-loader');
-const packageDef = protoLoader.loadSync('client_management.proto');
-const grpcObject = grpc.loadPackageDefinition(packageDef);
-const clientManagementPackage = grpcObject.clientmanagement;
+const grpc = require('@grpc/grpc-js');
+const services = require('./clientmanagement_grpc_pb.js');
+const messages = require('./clientmanagement_pb.js');
 
-const client = new clientManagementPackage.ClientManager('localhost:50051', grpc.credentials.createInsecure());
+const client = new services.ClientManagerClient(
+  'localhost:50051',
+  grpc.credentials.createInsecure()
+);
 
 // Exemple de création d'un client
-client.CreateClient({ id: '1', name: 'John Doe', email: 'john@example.com' }, (error, response) => {
-  console.log('Création:', response.message);
+let newClient = new messages.Client();
+newClient.setId('1');
+newClient.setName('John Doe');
+newClient.setEmail('johndoe@example.com');
+
+client.createClient(newClient, (error, response) => {
+  if (error) console.error(error);
+  console.log(response.getMessage());
 });
 
 // Exemple de récupération d'un client
-client.GetClient({ id: '1' }, (error, response) => {
-  console.log('Client:', response);
+let clientRequest = new messages.ClientRequest();
+clientRequest.setId('1');
+
+client.getClient(clientRequest, (error, response) => {
+  if (error) console.error(error);
+  console.log('Client:', response.toObject());
 });
+
+// Ajoutez ici des exemples pour updateClient et deleteClient si nécessaire
