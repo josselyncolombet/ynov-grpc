2aeb671a0db23bc37f6a734f2c932b9984e8fac9
Joss <joss@MacBook-Pro-de-Joss.local>
2023-11-21 10:01:01 +0100

    2nd commit

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-const grpc = require('grpc');
-const protoLoader = require('@grpc/proto-loader');
-const packageDef = protoLoader.loadSync('client_management.proto');
-const grpcObject = grpc.loadPackageDefinition(packageDef);
-const clientManagementPackage = grpcObject.clientmanagement;
+const grpc = require('@grpc/grpc-js');
+const services = require('./clientmanagement_grpc_pb.js');
+const messages = require('./clientmanagement_pb.js');
+
+const clients = {}; // Stockage simple des clients
+
+function createClient(call, callback) {
+  const client = call.request;
+  clients[client.getId()] = client;
+  callback(null, new messages.ClientResponse({ message: 'Client créé' }));
+}
+
+function getClient(call, callback) {
+  callback(null, clients[call.request.getId()]);
+}
+
+function updateClient(call, callback) {
+  const client = call.request;
+  clients[client.getId()] = client;
+  callback(null, new messages.ClientResponse({ message: 'Client mis à jour' }));
+}
+
+function deleteClient(call, callback) {
+  delete clients[call.request.getId()];
+  callback(null, new messages.ClientResponse({ message: 'Client supprimé' }));
+}
 
 const server = new grpc.Server();
-const clients = {}; // Simuler une base de données
-
-server.addService(clientManagementPackage.ClientManager.service, {
-  CreateClient: (call, callback) => {
-    const client = call.request;
-    clients[client.id] = client;
-    callback(null, { message: 'Client créé avec succès' });
-  },
-  GetClient: (call, callback) => {
-    callback(null, clients[call.request.id]);
-  },
-  UpdateClient: (call, callback) => {
-    const client = call.request;
-    clients[client.id] = client;
-    callback(null, { message: 'Client mis à jour avec succès' });
-  },
-  DeleteClient: (call, callback) => {
-    delete clients[call.request.id];
-    callback(null, { message: 'Client supprimé avec succès' });
-  }
+server.addService(services.ClientManagerService, {
+  createClient: createClient,
+  getClient: getClient,
+  updateClient: updateClient,
+  deleteClient: deleteClient
 });
 
-server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-console.log('Server running at http://0.0.0.0:50051');
-server.start();
+server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+  server.start();
+  console.log('Server running on port 50051');
+});
